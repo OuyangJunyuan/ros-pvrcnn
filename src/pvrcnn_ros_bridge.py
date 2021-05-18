f5d3112329bcd76589c28c2faae14232dbe9ca65
ouyjy <706545330@qq.com>
2021-05-18 20:44:42 +0800

    5-18

--- src/pvrcnn_ros_bridge.py
+++ src/pvrcnn_ros_bridge.py
@@ -7,7 +7,10 @@ import numpy as np
 import torch
 
 import rospy
+import actionlib
+from ros_pvrcnn.msg import detector3dAction, detector3dResult, box3d
 import ros_numpy as rnp
+from std_msgs.msg import String
 from sensor_msgs.msg import PointCloud2
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Point
@@ -75,6 +78,7 @@ class pvrcnn_ros:
         # pvrcnn cfg
         cfg_from_yaml_file(input_dict.cfg_file, cfg)
 
+        # create logger
         log_dir = Path(str(input_dict.output_dir)) / 'log'
         log_dir.mkdir(parents=True, exist_ok=True)
 
@@ -82,6 +86,7 @@ class pvrcnn_ros:
             log_dir / ('log_%s.txt' % datetime.datetime.now().strftime('%Y%m%d-%H%M%S')), rank
             =cfg.LOCAL_RANK)
 
+        # build dataset and network
         self.demo_dataset = DemoDataset(  # dummy dataset for preprocess inputdata
             dataset_cfg=cfg.DATA_CONFIG, class_names=cfg.CLASS_NAMES, training=False,
             root_path=input_dict.dummy_cloud,
@@ -94,47 +99,66 @@ class pvrcnn_ros:
 
         self.model.to(self.device)
         self.model.eval()
+        self.score_threshold = input_dict.score_threashold
 
         # for ROS
-        self.sub = rospy.Subscriber(input_dict.topic, PointCloud2, self.pc2_callback, queue_size=1)
+        self.action_server = actionlib.SimpleActionServer("excavator/lidar_perception/ros_pvrcnn_action",
+                                                          detector3dAction,
+                                                          execute_cb=self.execute_cb, auto_start=False)
+        self.action_server.start()
         self.mk_pub = rospy.Publisher("ros_pvrcnn", MarkerArray, queue_size=1)
-        self.frame_id = 0
+        self.cls_list = [String(cls) for cls in cfg.CLASS_NAMES]
 
     @exetime
     def detector(self, points):
         with torch.no_grad():
+            # prepare data and load data to gpu
             input_dict = {
                 'points': points,
-                'frame_id': self.frame_id,
+                'frame_id': 0,
             }
             data_dict = self.demo_dataset.prepare_data(data_dict=input_dict)
             data_dict = self.demo_dataset.collate_batch([data_dict])
             load_data_to_gpu(data_dict)
 
-            pred_dicts = self.model.forward(data_dict)[0][0]  # batch_size = 1
-            # print(pred_dicts['pred_boxes'].shape)
-            self.viz(pred_dicts['pred_boxes'].detach().cpu().numpy(), "excavator/LiDAR_80_1")
-            print(pred_dicts, cfg.CLASS_NAMES)
-            # pred = self.remove_low_score(pred_dicts[0])
+            # inference once in batch size : 1
+            pred_dicts = self.model.forward(data_dict)[0][0]
 
-            # mea_data = {}
-            # mea_data["time_stamp"] = pointcloud_msg.header.stamp.to_sec()
-            # mea_data["pointcloud"] = self.pointcloud
-            # mea_data["boxes_3d"] = pred[
-            #     "pred_boxes"].detach().cpu().numpy()  # boxes3d:  (N, 7) [x, y, z, dx, dy, dz, heading], (x, y, z) is the box center
-            # # mea_data["boxes_3d_scores"] = pred["pred_scores"].detach().cpu().numpy()
-            # # mea_data["boxes_3d_types"] = pred["pred_labels"].detach().cpu().numpy()
-            # self.det_3dbbox.append(mea_data)
-            # self.det_pc_handling = False
+            # analysis the result
+            pred_scores = pred_dicts['pred_scores'].detach().cpu().numpy()
+            indices = pred_scores > self.score_threshold
+
+            pred_scores = pred_scores[indices]
+            pred_boxes = pred_dicts['pred_boxes'].detach().cpu().numpy()[indices]
+            pred_labels = pred_dicts['pred_labels'].detach().cpu().numpy()[indices]
+            self.viz(pred_boxes, "excavator/LiDAR_80_1")
+            return pred_boxes, pred_scores, pred_labels
 
     @exetime
-    def pc2_callback(self, msg):
-        points_raw = rnp.point_cloud2.pointcloud2_to_xyz_array(msg)
-        points_raw[:, 2] = points_raw[:, 2]
-        points_raw = np.hstack((points_raw, np.zeros([len(points_raw), 1])))
-        self.detector(points_raw)
-        self.frame_id += 1
+    def execute_cb(self, request):
         print('-------------------------------------')
+        print(request.point_cloud.header.seq)
+
+        header, points_msg = request.point_cloud.header, request.point_cloud
+        points_raw = rnp.point_cloud2.pointcloud2_to_xyz_array(points_msg)
+        points_raw = np.hstack((points_raw, np.zeros([len(points_raw), 1])))
+
+        boxes, scores, labels = self.detector(points_raw)
+
+        result = detector3dResult()
+        result.header = header
+        result.cls_list = self.cls_list
+        # print(result.cls_list)
+        for box, label, score in zip(boxes, labels, scores):
+            box_ins = box3d()
+            box_ins.id = label - 1
+            box_ins.cls = cfg.CLASS_NAMES[box_ins.id]
+            box_ins.score = score
+            box_ins.xyz = box[0:3]
+            box_ins.lwh = box[3:6]
+            box_ins.heading = box[6]
+            result.result.append(box_ins)
+        self.action_server.set_succeeded(result)
 
     def viz(self, bbox3d, frame_id):
         marker_array = MarkerArray()
@@ -182,3 +206,117 @@ class pvrcnn_ros:
         self.mk_pub.publish(marker_array)
         marker_array.markers = []
         pass
+
+# for ros topic interface
+# class pvrcnn_ros:
+#     def __init__(self, input_dict):
+#         # pvrcnn cfg
+#         cfg_from_yaml_file(input_dict.cfg_file, cfg)
+#
+#         log_dir = Path(str(input_dict.output_dir)) / 'log'
+#         log_dir.mkdir(parents=True, exist_ok=True)
+#
+#         self.logger = common_utils.create_logger(
+#             log_dir / ('log_%s.txt' % datetime.datetime.now().strftime('%Y%m%d-%H%M%S')), rank
+#             =cfg.LOCAL_RANK)
+#
+#         self.demo_dataset = DemoDataset(  # dummy dataset for preprocess inputdata
+#             dataset_cfg=cfg.DATA_CONFIG, class_names=cfg.CLASS_NAMES, training=False,
+#             root_path=input_dict.dummy_cloud,
+#             ext='.bin', logger=self.logger
+#         )
+#         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#         self.model = build_network(model_cfg=cfg.MODEL, num_class=len(cfg.CLASS_NAMES), dataset=self.demo_dataset)
+#         self.model.load_params_from_file(filename=input_dict.ckpt_file, logger=self.logger,
+#                                          to_cpu=self.device == "cpu")
+#
+#         self.model.to(self.device)
+#         self.model.eval()
+#
+#         # for ROS
+#         self.sub = rospy.Subscriber(input_dict.topic, PointCloud2, self.pc2_callback, queue_size=1)
+#         self.mk_pub = rospy.Publisher("ros_pvrcnn", MarkerArray, queue_size=1)
+#         self.frame_id = 0
+#
+#     @exetime
+#     def detector(self, points):
+#         with torch.no_grad():
+#             input_dict = {
+#                 'points': points,
+#                 'frame_id': self.frame_id,
+#             }
+#             data_dict = self.demo_dataset.prepare_data(data_dict=input_dict)
+#             data_dict = self.demo_dataset.collate_batch([data_dict])
+#             load_data_to_gpu(data_dict)
+#
+#             pred_dicts = self.model.forward(data_dict)[0][0]  # batch_size = 1
+#             # print(pred_dicts['pred_boxes'].shape)
+#             self.viz(pred_dicts['pred_boxes'].detach().cpu().numpy(), "excavator/LiDAR_80_1")
+#             print(pred_dicts, cfg.CLASS_NAMES)
+#             # pred = self.remove_low_score(pred_dicts[0])
+#
+#             # mea_data = {}
+#             # mea_data["time_stamp"] = pointcloud_msg.header.stamp.to_sec()
+#             # mea_data["pointcloud"] = self.pointcloud
+#             # mea_data["boxes_3d"] = pred[
+#             #     "pred_boxes"].detach().cpu().numpy()  # boxes3d:  (N, 7) [x, y, z, dx, dy, dz, heading], (x, y, z) is the box center
+#             # # mea_data["boxes_3d_scores"] = pred["pred_scores"].detach().cpu().numpy()
+#             # # mea_data["boxes_3d_types"] = pred["pred_labels"].detach().cpu().numpy()
+#             # self.det_3dbbox.append(mea_data)
+#             # self.det_pc_handling = False
+#
+#     @exetime
+#     def pc2_callback(self, msg):
+#         points_raw = rnp.point_cloud2.pointcloud2_to_xyz_array(msg)
+#         points_raw[:, 2] = points_raw[:, 2]
+#         points_raw = np.hstack((points_raw, np.zeros([len(points_raw), 1])))
+#         self.detector(points_raw)
+#         self.frame_id += 1
+#         print('-------------------------------------')
+#
+#     def viz(self, bbox3d, frame_id):
+#         marker_array = MarkerArray()
+#         marker = Marker()
+#         marker.header.frame_id = frame_id
+#         marker.type = marker.LINE_LIST
+#         marker.action = marker.ADD
+#         marker.header.stamp = rospy.Time.now()
+#
+#         # marker scale (scale y and z not used due to being linelist)
+#         marker.scale.x = 0.08
+#         # marker color
+#         marker.color.a = 1.0
+#         marker.color.r = 1.0
+#         marker.color.g = 1.0
+#         marker.color.b = 0.0
+#
+#         marker.pose.position.x = 0.0
+#         marker.pose.position.y = 0.0
+#         marker.pose.position.z = 0.0
+#
+#         marker.pose.orientation.x = 0.0
+#         marker.pose.orientation.y = 0.0
+#         marker.pose.orientation.z = 0.0
+#         marker.pose.orientation.w = 1.0
+#         marker.points = []
+#         corner_for_box_list = [0, 1, 0, 3, 2, 3, 2, 1, 4, 5, 4, 7, 6, 7, 6, 5, 3, 7, 0, 4, 1, 5, 2, 6]
+#         corners3d = boxes_to_corners_3d(bbox3d)  # (N,8,3)
+#         for box_nr in range(corners3d.shape[0]):
+#             box3d_pts_3d_velo = corners3d[box_nr]  # (8,3)
+#             for corner in corner_for_box_list:
+#                 transformed_p = np.array(box3d_pts_3d_velo[corner, 0:4])
+#                 # transformed_p = transform_point(p, np.linalg.inv(self.Tr_velo_kitti_cam))
+#                 p = Point()
+#                 p.x = transformed_p[0]
+#                 p.y = transformed_p[1]
+#                 p.z = transformed_p[2]
+#                 marker.points.append(p)
+#         marker_array.markers.append(marker)
+#
+#         id = 0
+#         for m in marker_array.markers:
+#             m.id = id
+#             id += 1
+#         self.mk_pub.publish(marker_array)
+#         marker_array.markers = []
+#         pass
--- src/pvrcnn_ros_server.py
+++ src/pvrcnn_ros_server.py
@@ -1,7 +1,10 @@
-import rospy
-import rospkg
+#!/home/ou/software/anaconda3/envs/pvrcnn/bin/python
 from pathlib import Path
 from easydict import EasyDict
+
+import rospy
+import rospkg
+
 from pvrcnn_ros_bridge import pvrcnn_ros
 
 root_path = Path(rospkg.RosPack().get_path("ros_pvrcnn"))
@@ -10,12 +13,16 @@ if __name__ == "__main__":
     rospy.init_node('ros_pvrcnn_node')
 
     input_dict = EasyDict()
-    input_dict.output_dir = root_path / "output"
-    input_dict.cfg_file = root_path / "config/pv_rcnn.yaml"
-    input_dict.ckpt_file = root_path / "config/pv_rcnn_8369.pth"
-    input_dict.dummy_cloud = root_path / "config/000000.bin"
-    input_dict.topic = "/excavator/LiDAR_80_1"
+    # input_dict.output_dir = root_path / "output"
+    # input_dict.cfg_file = root_path / "config/ironpile/stone_pv_rcnn.yaml"
+    # input_dict.ckpt_file = root_path / "config/ironpile/checkpoint_epoch_80.pth"
+    # input_dict.dummy_cloud = root_path / "config/000000.bin"
 
+    input_dict.output_dir = root_path / "output"
+    input_dict.cfg_file = root_path / "config/person_truck/person_truck_pv_rcnn.yaml"
+    input_dict.ckpt_file = root_path / "config/person_truck/person_truck_checkpoint_epoch_80.pth"
+    input_dict.dummy_cloud = root_path / "config/000000.bin"
+    input_dict.score_threashold = 0.5
     detector = pvrcnn_ros(input_dict)
 
     rospy.spin()
